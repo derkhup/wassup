1a34a0a4311907a2828878ab76b00791d1f537e0
Your Name <you@example.com>
2017-10-24 16:33:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    
+    $dagen = array("zondag","maandag","dinsdag","woensdag","donderdag","vrijdag","zaterdag" );
+    $teller = array ("1","2","3","4","5","6","7");
+    
+    
+         for($i = 0; $i < count($dagen); $i++){
+            echo "NIEWE REGEL IN ARRAYCOUT : ";
+            echo $dagen[$i];
+            echo $teller [$i];
+            echo $i;
+            echo "<br>";
+         
+         }
+
+   
+ ?>
